4d89fa1e4a0f27e76bee8a68bc2faa223c6a5650
ynfeng <fynwin@gmail.com>
2021-06-16 17:45:13 +0800

    添加书上的示例代码.

---
+++ src/main/java/com/github/ynfeng/refactor/change_function_declaration/Circle.java
@@ -0,0 +1,8 @@
+package com.github.ynfeng.refactor.change_function_declaration;
+
+public class Circle {
+
+    public static double circum(double radius) {
+        return 2 * Math.PI * radius;
+    }
+}
