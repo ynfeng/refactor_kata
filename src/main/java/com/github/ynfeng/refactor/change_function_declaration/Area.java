46aac28b4abc0576566385e54e80cf9af13d6427
ynfeng <fynwin@gmail.com>
2021-06-16 18:45:58 +0800

    修改注释.

--- src/main/java/com/github/ynfeng/refactor/change_function_declaration/Area.java
+++ src/main/java/com/github/ynfeng/refactor/change_function_declaration/Area.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Area {
     /**
-     * 接收state参数
+     * 改为接收Customer.address.state参数
      */
     public static boolean inNewEngland(Customer customer) {
         return Arrays.asList("MA","CT","ME","VT","NH","RI")
