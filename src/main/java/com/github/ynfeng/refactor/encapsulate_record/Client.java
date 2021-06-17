3d8a52d21b2633d873a690582fd441198c7c4f28
ynfeng <fynwin@gmail.com>
2021-06-17 17:11:51 +0800

    封装记录.

---
+++ src/main/java/com/github/ynfeng/refactor/encapsulate_record/Client.java
@@ -0,0 +1,18 @@
+package com.github.ynfeng.refactor.encapsulate_record;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Client {
+    private static final Map<String, String> organization = new HashMap<>();
+
+    static {
+        organization.put("name", "Acme Gooseberries");
+        organization.put("country", "GB");
+    }
+
+    public static void main(String[] args) {
+        String result = String.format("<h1>%s</h1>", organization.get("name"));
+        organization.put("name", "newName");
+    }
+}
